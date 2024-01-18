f0e9dc1382d7ce3dd7be42ea57d3309d0c9e9ca7
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-18 15:18:34 +0100

    fixed task 7 error

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,12 +2,11 @@
 -- Requirements:
     -- Procedure ComputeAverageScoreForUser is taking 1 input:
     -- user_id, a users.id value (you can assume user_id is linked to an existing users)
-DROP procedure IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$ ;
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (SELECT AVG(score) FROM corrections
-                WHERE user_id = corrections.user_id);
+                WHERE user_id=corrections.user_id);
 END;$$
 DELIMITER ;
