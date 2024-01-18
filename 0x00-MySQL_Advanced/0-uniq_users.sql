b3f9f9c688c88be9c38f3003d93cddfc2692dcea
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-18 15:15:28 +0100

    fixed checker error for task 0, 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,9 +3,6 @@
     -- email: string (255 characters), never null and unique
     -- name: string (255 characters)
 
-CREATE DATABASE IF NOT EXISTS holberton;
-
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,9 +4,6 @@
     -- name: string (255 characters)
     -- country:  enumeration of countries: US, CO and TN, never null
 
-CREATE DATABASE IF NOT EXISTS holberton;
-
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
