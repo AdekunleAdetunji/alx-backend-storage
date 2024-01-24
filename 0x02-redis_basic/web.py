adc7aa48d5a24674801a9232a753567ab3c2f3fa
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-24 23:38:34 +0100

    corrected pycodestyle error

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,20 +5,20 @@ page is accessed
 """
 import redis
 import requests
-r = redis.Redis()
+
 count = 0
 
 
 def get_page(url: str) -> str:
     """
     Track how many times a web page is accessed
-    
+
     Args:
         url: The uniform resource locator to reach the website
-    
+
     Return: count of how many time the web page is accessed in ten seconds
     """
-    r.set(f"cached:{url}", count)
+    r = redis.Redis()
     resp = requests.get(url)
     r.incr(f"count:{url}")
     r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
