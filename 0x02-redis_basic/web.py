8d4157acecb7d2d8c7a9457286bc169c67abf955
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-24 23:26:58 +0100

    solution to x02-redis_basic advanced task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contains a function get_page that tracks the number of times a web
+page is accessed
+"""
+import redis
+import requests
+r = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """
+    Track how many times a web page is accessed
+    
+    Args:
+        url: The uniform resource locator to reach the website
+    
+    Return: count of how many time the web page is accessed in ten seconds
+    """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
